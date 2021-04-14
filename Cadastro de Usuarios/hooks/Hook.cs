7320000df7d9316802d857f87b780d311fc4b9aa
Gabriela Ambos <gabrielaambos15@gmail.com>
2021-04-13 23:44:29 -0300

    Adicionando o projeto ao repositório

---
+++ Cadastro de Usuarios/hooks/Hook.cs
@@ -0,0 +1,21 @@
+﻿using Cadastro_de_Usuarios.utils;
+using TechTalk.SpecFlow;
+
+namespace Cadastro_de_Usuarios.hooks
+{
+    [Binding]
+    public sealed class Hook
+    {
+        [Before]
+        public void Setup()
+        {
+            Browser.SetCurrentBrowser(Browser.Type.CHROME);
+        }
+
+        [After]
+        public void TearDown()
+        {
+            Browser.QuitDriver();
+        }
+    }
+}
